4880bf37013de74103ad349126d3366659f780a9
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-03 08:37:00 +0300

    Added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      {/* Navbar component */}
+      <nav className="NAVBAR">
+        {/* Container for navigation links */}
+        <div className="navbar-links">
+          {/* Link to homepage */}
+          <Link to="/">
+            {/* Logo image */}
+            <img
+              alt=""
+              src="https://i.pinimg.com/236x/4f/a8/b7/4fa8b7ae78cc4ed871f29c768f533397.jpg"
+              className="img-nav"
+            />
+          </Link>
+          {/* Link to animal list page */}
+          <Link className="img-nav" to="/animalList">
+            FIND PET
+          </Link>
+          {/* Link to organization list page */}
+          <Link className="img-nav" to="/organizationList">
+            <span>ORGANISATIONS </span>
+          </Link>
+          {/* Link to sign up page */}
+          <Link className="img-nav" to="/login">
+            SIGN UP
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
