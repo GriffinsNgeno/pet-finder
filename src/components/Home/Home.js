7a3f934b301b41e61b6a93e941880ad8a8ac101f
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-04 16:55:45 +0300

    updated home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,8 +11,6 @@ function Home() {
           <img src="https://images.unsplash.com/photo-1583511666372-62fc211f8377?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fHBldHN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" alt="img" />
         </div>
         <div className="text">
-          {/* Emojis */}
-          👩🏼‍🦰 🧑🏼‍🦰
           {/* Header component */}
           <h1 className="mega-font">
             Hello
@@ -23,30 +21,6 @@ function Home() {
           Petfinder is a company dedicated to helping people find their perfect pet match. The company was founded with the mission of promoting the adoption of homeless pets and reducing pet overpopulation. 
           Petfinder operates as an online database that connects potential adopters with animals available for adoption at animal shelters and rescue organizations across the Kenya🇰🇪.
           </p>
-
-          {/* Empty paragraph component */}
-          <p className="number"></p>
-          {/* Horizontal line component */}
-          <hr width="50%" />
-          
-          {/* Column grid component */}
-          <div className="column-grid">
-            <div className="column-1">
-              {/* Column component */}
-              <h3>Dogs</h3>
-              <p>Domestic Animals</p>
-            </div>
-            <div className="column-2">
-              {/* Column component */}
-              <h3>Cats</h3>
-              <p>Tamed Animals</p>
-            </div>
-            <div className="column-3">
-              {/* Column component */}
-              <h3>Family Friendly</h3>
-              <p>kids safe</p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
