933b7561602e55ea2fca6ba6cbbcfe68def106d8
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-04 16:55:35 +0300

    Updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,18 @@ import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home";
 import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import AnimalList from "./AnimalList";
 
 function App() {
+  
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/animalList" element={<AnimalList />}></Route>
       </Routes>
        <Footer />
     </div>
